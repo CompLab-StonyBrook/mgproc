5b8be09a43276eaff76cb293efa48886273f4e9f
Thomas Graf <git@thomasgraf.net>
2017-02-14 01:23:36 -0500

    add texprint for producing complete forest files

--- helpers.py
+++ helpers.py
@@ -52,7 +52,7 @@ def named(tree: 'GornTree', address: str) -> str:
 
 
 @int2str
-def forest(tree: 'IOTree', address: str) -> str:
+def forest(tree: 'IOTree', address: str, macros: bool=False) -> str:
     """Prints label of tree node in forest notation."""
     # make sure tree has been parsed; needed for index and outdex
     node = tree.struct[address]
@@ -61,26 +61,39 @@ def forest(tree: 'IOTree', address: str) -> str:
     index = node.index()
     outdex = node.outdex()
 
-    # decide what LaTeX macro to use
-    if outdex - index > 2 and not node.leaf:
-        node_type = 'IBLab'
-    elif outdex - index > 2:
-        node_type = 'BLab'
-    else:
-        node_type = 'Lab'
+    empty = ', empty' if node.empty else ''
 
-    return (
-        # start the LaTeX macro
-        '\\' + node_type +
-        # first macro argument: node label
-        '{' + node.label() + '}' +
-        # second macro argument: index
-        '{' + str(node.index()) + '}' +
-        # third macro argument: outdex
-        '{' + str(node.outdex()) + '}' +
-        # add name for movement arrows and other decorations
-        ', name=' + node.name()
-        )
+    # decide what LaTeX macro to use
+    if macros:
+        if outdex - index > 2 and not node.leaf:
+            node_type = 'IBLab'
+        elif outdex - index > 2:
+            node_type = 'BLab'
+        else:
+            node_type = 'Lab'
+        return (
+            # start the LaTeX macro
+            '\\' + node_type +
+            # first macro argument: node label
+            '{' + node.label() + '}' +
+            # second macro argument: index
+            '{' + str(node.index()) + '}' +
+            # third macro argument: outdex
+            '{' + str(node.outdex()) + '}' +
+            # add name for movement arrows and other decorations
+            ', name=' + node.name() +
+            # add property empty if necessary
+            empty
+            )
+    else:
+        return (
+            # node label
+            node.label() +
+            # add name for movement arrows and other decorations
+            ', name=' + node.name() +
+            # add property empty if necessary
+            empty
+            )
 
 
 def ioprint(tree: 'IOTree', extension: str='.io.forest',
@@ -88,28 +101,64 @@ def ioprint(tree: 'IOTree', extension: str='.io.forest',
     """Prints index/outdex annotation as tikz nodes."""
     string = ''
     command_padding = 24
-    label_padding = 8
+    label_padding = 24
+    node_padding = 5
 
     for node in tree.struct.values():
-        node_name = node.name()
-        node_index = node.index()
-        node_outdex = node.outdex()
+        node_name = ('(' + node.name() + ')').ljust(label_padding)
+        node_index = ('{' + str(node.index()) + '}').rjust(node_padding)
+        node_outdex = ('{' + str(node.outdex()) + '}').rjust(node_padding)
 
         # draw a tikz node for index
         index_string = '\\node[index]'.ljust(command_padding)
-        index_node = ('{' + str(node_index) + '}').ljust(label_padding)
-        index_string += 'at ({0}) {1};\n'.format(node_name, index_node)
+        index_string += 'at {0} {1};\n'.format(node_name, node_index)
 
         # draw a tikz node for outdex
-        node_option = ', boxed]' if node_outdex - node_index > 2 else ']'
+        node_option = ', boxed]' if node.outdex() - node.index() > 2 else ']'
         outdex_string = ('\\node[outdex' + node_option).ljust(command_padding)
-        outdex_node = ('{' + str(node_outdex) + '}').ljust(label_padding)
-        outdex_string += '{1} at ({0});\n'.format(node_name, outdex_node)
+        outdex_string += 'at {0} {1};\n'.format(node_name, node_outdex)
+
+        # put them together
         string += index_string + outdex_string + '%\n'
 
     # forest chokes on empty lines,
     # so add a % after the very last \n
     string += '%'
+
+    if not filename:
+        return string
+    else:
+        if not directory:
+            directory = '.'
+        filename += extension
+        filename = os.path.join(directory, filename)
+        with open(filename, 'w') as text_file:
+            print(string, file=text_file)
+
+
+def texprint(tree: 'IOTree', extension: str='.mgproc.forest',
+             filename: str=None, directory: str=None,
+             tree_directory: str=None,
+             io: bool=True) -> str:
+    """Prints forest file of tree"""
+    string = '\\begin{forest}'
+    tree_header = '\n%\n' + '%' * 8 + '\n% Tree %\n' + '%' * 8
+    string += tree_header + '\n%\n' + tree.print(annotation=forest)
+
+    # add move specification
+    move = os.path.join(tree_directory, tree.name) if tree_directory else tree.name
+    move_header = '%' * 10 + '\n% Movers %\n' + '%' * 10
+    with open(move + '.move.forest', 'r') as move_file:
+        string += '\n%\n' + move_header + '\n%\n' + move_file.read() + '%'
+        move_file.close()
+
+    # add index/outdex annotation if requested
+    if io:
+        io_header = '%' * 15 + '\n% Annotations %\n' + '%' * 15
+        string += '\n' + io_header + '\n%\n' + ioprint(tree)
+
+    # and close the forest environment
+    string += '\n\\end{forest}'
 
     if not filename:
         return string
